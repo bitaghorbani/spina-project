d5d047941af4dbd26944f6e73bf7640e9d9e0121
bita ghorbani <bitaghorbani787@gmail.com>
2023-08-12 20:06:38 +0330

    added string+number

--- main.js
+++ main.js
@@ -14,3 +14,10 @@ const number2 = 2;
 let sum = number1 + number2;
 console.log(sum);
 
+/*   ٪ باقی مانده تقسیم
+     / خارج قسمت تقسیم
+*/
+let i = "3" + 2 + 4;
+console.log(i);
+i = 2 + 4 + "3";
+console.log(i);
