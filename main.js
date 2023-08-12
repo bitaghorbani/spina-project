b3f295e3fda50aa693a91d5b99a1b5b4ee5e040c
bita ghorbani <bitaghorbani787@gmail.com>
2023-08-12 19:55:02 +0330

    added const

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
-let firstName = "Bita";
-let lastName = "Ghorbani";
-console.log(firstName,lastName);
+let f = "Bita";
+let l = "Ghorbani";
+console.log(f,l);
+
+let x;
+let y = 2;
+x = 3;
+let z = x + 2;
+console.log(z);
+
+//const u;
+const number1 = 4;
+const number2 = 2;
+let sum = number1 + number2;
+console.log(sum);
+
