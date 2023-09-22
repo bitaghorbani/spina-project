6432ebb605b6924dd7bc12a5d1da82a8858c25f1
bita ghorbani <bitaghorbani787@gmail.com>
2023-09-22 19:42:05 +0330

    added menuCoffee

---
+++ main1.js
@@ -0,0 +1,18 @@
+// const coffee = ["اسپرسو", "کاپوچینو", "هات چاکلت", "قهوه ترک" ];
+// console.log(coffee);
+// console.log("length of array:", coffee.length)
+// console.log("____________________________");
+
+// const c = coffee[0];
+// console.log(c);
+// console.log("____________________________");
+// coffee.forEach(item => console.log(item));
+// console.log("____________________________");
+// coffee.forEach(item => console.log(item.length));
+// console.log("____________________________");
+
+// let spanElement = '';
+// coffee.forEach((item, index) => {
+//      spanElement += "<span>" + item + "</span><br/>"
+// });
+// console.log(spanElement);
